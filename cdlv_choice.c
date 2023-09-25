0d7a38b1c3002c3be0b0330d9f2386181ea9918a
armbp <mar@armbp>
2023-09-25 22:15:22 +0200

    rebase

---
+++ cdlv_choice.c
@@ -0,0 +1,55 @@
+#include "cdlv.h"
+#include "cdlv_util.h"
+
+void cdlv_choice_create(cdlv_base* base) {
+    base->choice = malloc(sizeof(cdlv_choice));
+    if(!base->choice)
+        cdlv_die("Could not allocate memory for a new scripted choice!");
+
+    base->choice->count               = 0;
+    base->choice->current             = 0;
+    base->choice->state               = cdlv_parsing;
+    base->choice->destinations        = NULL;
+    base->choice->options             = NULL;
+    base->choice->prompt              = NULL;
+    cdlv_alloc_ptr_arr(&base->choice->destinations, cdlv_max_choice_count, size_t);
+    cdlv_alloc_ptr_arr(&base->choice->options, cdlv_max_choice_count, char*);
+    cdlv_alloc_ptr_arr(&base->choice->prompt, cdlv_max_string_size, char);
+}
+
+static inline void cdlv_choice_clean(cdlv_base* base) {
+    for(size_t i=0;i<cdlv_max_choice_count; ++i)
+        if(base->choice->options[i]) free(base->choice->options[i]);
+    free(base->choice->options);
+    free(base->choice->prompt);
+    free(base->choice->destinations);
+    free(base->choice);
+    base->choice = NULL;
+}
+
+void cdlv_choice_add(cdlv_base* base, const char* line) {
+    const char chars[12] = "1234567890 ";
+    size_t d = 0;
+    size_t s = 0;
+    if((sscanf(line, "%lu", &d)) != 0)
+        if(d < base->scene_count)
+            s = strspn(line, chars);
+    if(s>0 && base->choice->count < cdlv_max_choice_count) {
+        base->choice->destinations[base->choice->count] = d;
+        cdlv_duplicate_string(&base->choice->options[base->choice->count], line+s, strlen(line+s)+1);
+        ++base->choice->count;
+    }
+}
+
+static inline bool cdlv_choice_switch(cdlv_base* base, size_t ch) {
+    if(base->choice->destinations[ch]) {
+        cdlv_scene_load(base, base->c_scene, base->choice->destinations[ch]);
+        return true;
+    }
+    return false;
+}
+
+void cdlv_choice_handler(cdlv_base* base, size_t ch) {
+    if(cdlv_choice_switch(base, ch)) cdlv_choice_clean(base);
+}
+
---
+++ cdlv_util.h
@@ -0,0 +1,30 @@
+#ifndef CDLV_UTIL_H
+#define CDLV_UTIL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+#include "cdlv.h"
+#include "cdlv_macros.h"
+#include "cdlv_types.h"
+
+void cdlv_text_create(cdlv_base* base, const char* path, const size_t size, const uint32_t wrap, const size_t x, const size_t y, const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a, SDL_Renderer* renderer);
+void cdlv_text_render(cdlv_base* base, SDL_Renderer* r);
+void cdlv_text_update(cdlv_base* base, const char* content);
+
+void cdlv_choice_create(cdlv_base* base);
+void cdlv_choice_add(cdlv_base* base, const char* line);
+void cdlv_choice_handler(cdlv_base* base, size_t ch);
+
+void cdlv_scene_load(cdlv_base* base, const size_t prev, const size_t index);
+void cdlv_update(cdlv_base* base);
+void cdlv_handle_keys(cdlv_base* base, SDL_Event* e);
+void cdlv_canvas_create(cdlv_base* base, const size_t w, const size_t h, const size_t fps, SDL_Renderer** r);
+void cdlv_read_file(cdlv_base* base, const char* file, SDL_Renderer** r);
+
+#endif
