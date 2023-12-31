c986ac77541e0676d28325c5db49d0fcc793ad39
armbp <mar@armbp>
2023-12-31 16:13:24 +0100

    add -DZDLV_ONLY to cmake

--- zdlv.c
+++ zdlv.c
@@ -1,6 +1,3 @@
-#include "cdlv.h"
-#include "cdlv_util.h"
-#include "menu/cdlv_menu.h"
 #include "zocket/zocket.h"
 #include "zdlv.h"
 #include <sys/stat.h>
@@ -15,6 +12,55 @@ struct image {
     char* data;
     size_t size;
 };
+
+char** cdlv_read_file_in_lines(const char* path, size_t* line_count) {
+    FILE* file = fopen(path, "rb");
+    if(!file) {
+        perror("cdlv");
+        return NULL;
+    }
+
+    fseek(file, 0L, SEEK_END);
+    long size = ftell(file);
+    rewind(file);
+
+    char** code = malloc(size+1);
+    if(!code) {
+        fclose(file), cdlv_logv("Could not allocate memory for file: %s", path);
+        return NULL;
+    }
+    
+    char* temp = malloc(size+1);
+    if(!temp) {
+        fclose(file), cdlv_logv("Could not allocate memory for file: %s", path);
+        return NULL;
+    }
+
+    if(fread(temp, size, 1, file) != 1) {
+        fclose(file), free(temp), cdlv_logv("Could not read file: %s", path);
+        return NULL;
+    }
+    temp[size] = '\0';
+
+    char* line = strtok(temp, "\r\n");
+    size_t i = 0;
+    while(line) {
+        cdlv_duplicate_string(&code[i], line, strlen(line)+1);
+        ++i;
+        line = strtok(NULL, "\r\n");
+    }
+
+    if(feof(file)) fclose(file);
+    free(temp);
+
+    *line_count = i;
+    return code;
+}
+
+void cdlv_free_file_in_lines(char** file, const size_t line_count) {
+    for(size_t i=0; i<line_count; ++i) free(file[i]);
+    free(file);
+}
 
 static inline void write_file_zkt_data(const char* path, zkt_data* data) {
     FILE* file = fopen(path, "wb");
